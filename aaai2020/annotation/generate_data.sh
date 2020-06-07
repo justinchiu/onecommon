31af4efe2cb3a2d99f60c331a8a2e4bb30a8e346
Daniel Fried <dfried@berkeley.edu>
2020-06-06 18:26:44 -0700

    add annotation script and json

---
+++ aaai2020/annotation/generate_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# python reference_annotation.py --output_brat_format --success_only --correct_misspellings --replace_strings
+# python reference_annotation.py --output_brat_format --success_only --correct_misspellings --replace_strings --start_batch_index 30
+# python reference_annotation.py --output_markable_annotation
+# python reference_annotation.py --referent_aggregation
+
+for seed in `seq 0 9`; do
+  python transform_referents_to_txt.py --normalize --seed $seed
+done
+python transform_scenarios_to_txt.py --normalize --input_file ../experiments/data/onecommon/shared_4.json --output_file ../experiments/data/onecommon/shared_4.txt
+python transform_scenarios_to_txt.py --normalize --input_file ../experiments/data/onecommon/shared_5.json --output_file ../experiments/data/onecommon/shared_5.txt
+python transform_scenarios_to_txt.py --normalize --input_file ../experiments/data/onecommon/shared_6.json --output_file ../experiments/data/onecommon/shared_6.txt
