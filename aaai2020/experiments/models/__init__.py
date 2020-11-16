cd42be2b744d5e6d254fa280a75153f3d3a7dcf8
Daniel Fried <dfried@berkeley.edu>
2020-11-16 23:01:03 +0000

    .

--- aaai2020/experiments/models/__init__.py
+++ aaai2020/experiments/models/__init__.py
@@ -1,14 +1,18 @@
 from models.ctx_encoder import MlpContextEncoder, AttentionContextEncoder, RelationalAttentionContextEncoder, RelationalAttentionContextEncoder2, RelationalAttentionContextEncoder3
 from models.rnn_reference_model import RnnReferenceModel, HierarchicalRnnReferenceModel
+# from models.ctx_encoder_ua import AttentionContextEncoder as AttentionContextEncoderUA
+# from models.rnn_reference_model_ua import RnnReferenceModel as RnnReferenceModelUA
 
 MODELS = {
     'rnn_reference_model': RnnReferenceModel,
+    # 'rnn_reference_model_ua': RnnReferenceModelUA,
     'hierarchical_rnn_reference_model': HierarchicalRnnReferenceModel,
 }
 
 CTX_ENCODERS = {
     'mlp_encoder': MlpContextEncoder,
     'attn_encoder': AttentionContextEncoder,
+    # 'attn_encoder_ua': AttentionContextEncoderUA,
     'rel_attn_encoder': RelationalAttentionContextEncoder,
     'rel_attn_encoder_2': RelationalAttentionContextEncoder2,
     'rel_attn_encoder_3': RelationalAttentionContextEncoder3,
