9b6b9d2c103b6def53d6970db676fd2a19b1d107
Daniel Fried <dfried@berkeley.edu>
2020-11-18 20:50:59 +0000

    residuals

--- webapp/src/run_sample_2.sh
+++ webapp/src/run_sample_2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 export PYTHONPATH="../../aaai2020:../../aaai2020/experiments:../../aaai2020/experiments/models:..:$PYTHONPATH"
 
 scenarios="shared_4"
 
-python web/chat_app.py --port 5000 \
+python web/chat_app.py --port 3000 \
     --schema-path data/schema.json \
     --config web/app_params.json \
     --scenarios-path ../../aaai2020/experiments/data/onecommon/${scenarios}.json \
-    --output web/${scenarios} \
+    --output web/debug \
     $@
 
