af88815db34bd47689b51b2e0457cc7ea7ba98cf
Justin Chiu <justinchiu@g.harvard.edu>
2022-04-21 00:02:37 -0400

    improve logging of expected information gain

--- webapp/dialog_analysis/belief.py
+++ webapp/dialog_analysis/belief.py
@@ -59,19 +59,29 @@ class Belief:
             for x in range(2 ** num_dots)
         ])
 
-    def p_response(self, prior, ask):
+    def joint(self, prior, utt):
         raise NotImplementedError
 
-    def posterior(self, prior, ask, response):
+    def p_response(self, prior, utt):
         raise NotImplementedError
 
-    def info_gain(self, prior, ask, response):
+    def posterior(self, prior, utt, response):
+        raise NotImplementedError
+
+    def info_gain(self, prior, utt, response):
         Hs = entropy(prior)
-        Hs_r = entropy(self.posterior(prior, ask, response))
+        Hs_r = entropy(self.posterior(prior, utt, response))
         return Hs - Hs_r
 
-    def expected_info_gain(self, prior, ask):
+    def expected_info_gain(self, prior, utt):
         raise NotImplementedError
+
+    def viz_belief(self, p, n=5):
+        # decreasing order
+        idxs = (-p).argsort()[:n]
+        cs = self.configs[idxs]
+        ps = p[idxs]
+        return cs, ps
 
 
 class IndependentBelief(Belief):
@@ -98,35 +108,35 @@ class IndependentBelief(Belief):
         correct = 0.9
         error = 1 - correct
         likelihood = np.ones((2,2,2)) * error
-        # ask about something, get correct answer
+        # utt about something, get correct answer
         likelihood[1,1,1] = correct
         likelihood[0,1,0] = correct
-        # if you dont ask about something, no change
+        # if you dont utt about something, no change
         likelihood[:,0] = 1
         self.likelihood = likelihood
 
     # RESPONSE IS FOR ALL DOTS INDEPENDENTLY
-    def p_response(self, prior, ask):
-        return (self.likelihood[:,ask] * prior).sum(-1).T
+    def p_response(self, prior, utt):
+        return (self.likelihood[:,utt] * prior).sum(-1).T
 
-    def posterior(self, prior, ask, response):
-        f = self.likelihood[response, ask] * prior
+    def posterior(self, prior, utt, response):
+        f = self.likelihood[response, utt] * prior
         return f / f.sum(-1, keepdims=True)
 
-    def info_gain(self, prior, ask, response):
+    def info_gain(self, prior, utt, response):
         Hs = entropy(prior)
-        Hs_r = entropy(self.posterior(prior, ask, response))
-        return (Hs - Hs_r)[ask.astype(bool)].sum()
+        Hs_r = entropy(self.posterior(prior, utt, response))
+        return (Hs - Hs_r)[utt.astype(bool)].sum()
 
-    def expected_info_gain(self, prior, ask):
-        p_response = self.p_response(prior, ask)
+    def expected_info_gain(self, prior, utt):
+        p_response = self.p_response(prior, utt)
         Hs = entropy(prior)
         r0 = np.zeros((self.num_dots,), dtype=int)
         r1 = np.ones((self.num_dots,), dtype=int)
-        Hs_r0 = entropy(self.posterior(prior, ask, r0))
-        Hs_r1 = entropy(self.posterior(prior, ask, r1))
+        Hs_r0 = entropy(self.posterior(prior, utt, r0))
+        Hs_r1 = entropy(self.posterior(prior, utt, r1))
         EHs_r = (p_response * np.stack((Hs_r0, Hs_r1), 1)).sum(-1)
-        return (Hs - EHs_r)[ask.astype(bool)].sum()
+        return (Hs - EHs_r)[utt.astype(bool)].sum()
 
 
 
@@ -156,34 +166,34 @@ class AndBelief(Belief):
         correct = 0.9
         error = 1 - correct
         likelihood = np.ones((2,2,2)) * error
-        # ask about something, get correct answer
+        # utt about something, get correct answer
         likelihood[1,1,1] = correct
         likelihood[0,1,0] = correct
-        # if you dont ask about something, no change
+        # if you dont utt about something, no change
         likelihood[:,0] = 1
         self.likelihood = likelihood
 
-    def p_response(self, prior, ask):
+    def p_response(self, prior, utt):
         # prior: num_configs * 7
         # \sum_s p(r=1 | u, s)p(s) = \sum_s \prod_i p(r=1 | ui, si)p(s)
         p_r1 = 0
         p_r0 = 0
         for s,p in enumerate(prior):
             likelihood = 1
-            for i,d in enumerate(ask):
+            for i,d in enumerate(utt):
                 likelihood *= self.likelihood[1,d,self.configs[s,i]]
             p_r1 += likelihood * p
             p_r0 += (1-likelihood) * p
         #p_r0 = 1 - p_r1 # this is equivalent
         return np.array((p_r0, p_r1))
 
-    def posterior(self, prior, ask, response):
+    def posterior(self, prior, utt, response):
         # p(r=., s | u) = \prod_i p(r=. | ui, si)p(s)
         p_r0s_u = []
         p_r1s_u = []
         for s,p in enumerate(prior):
             likelihood = 1
-            for i,d in enumerate(ask):
+            for i,d in enumerate(utt):
                 likelihood *= self.likelihood[1,d,self.configs[s,i]]
             p_r1s_u.append(likelihood * p)
             p_r0s_u.append((1-likelihood) * p)
@@ -195,16 +205,16 @@ class AndBelief(Belief):
         p_s_ur0 = p_r0s_u / Z2 if Z2 > 0 else np.ones((2 ** self.num_dots,)) / 2 ** self.num_dots
         return p_s_ur1 if response == 1 else p_s_ur0
 
-    def info_gain(self, prior, ask, response):
+    def info_gain(self, prior, utt, response):
         Hs = entropy(prior)
-        Hs_r = entropy(self.posterior(prior, ask, response))
+        Hs_r = entropy(self.posterior(prior, utt, response))
         return Hs - Hs_r
 
-    def expected_info_gain(self, prior, ask):
-        p_response = self.p_response(prior, ask)
+    def expected_info_gain(self, prior, utt):
+        p_response = self.p_response(prior, utt)
         Hs = entropy(prior)
-        Hs_r0 = entropy(self.posterior(prior, ask, 0))
-        Hs_r1 = entropy(self.posterior(prior, ask, 1))
+        Hs_r0 = entropy(self.posterior(prior, utt, 0))
+        Hs_r1 = entropy(self.posterior(prior, utt, 1))
         EHs_r = (p_response * np.array((Hs_r0, Hs_r1))).sum()
         return Hs - EHs_r
 
@@ -229,7 +239,7 @@ class AndOrBelief(AndBelief):
         for each dot in z
         Size of z: remaining dots outside of s |z| = num_dots - |s|
     """
-    def p_response(self, prior, ask):
+    def p_response(self, prior, utt):
         # prior: num_configs * 7
         # \sum_s p(r=1 | u, s)p(s)
         # = \sum_s,z p(s)p(z|s) \prod_i 1-p(r=0|ui,si)p(r=0|ui,z)
@@ -240,7 +250,7 @@ class AndOrBelief(AndBelief):
             likelihood = 1
             state_config = self.configs[s]
             z = self.num_dots - state_config.sum()
-            for i,d in enumerate(ask):
+            for i,d in enumerate(utt):
                 if d == 1:
                     disconfirm = self.likelihood[0,d,state_config[i]] * (8/9) ** z
                     likelihood *= 1 - disconfirm
@@ -249,7 +259,7 @@ class AndOrBelief(AndBelief):
         #p_r0 = 1 - p_r1 # this is equivalent
         return np.array((p_r0, p_r1))
 
-    def posterior(self, prior, ask, response):
+    def posterior(self, prior, utt, response):
         # p(r=., s | u) = \prod_i p(r=. | ui, si)p(s)
         p_r0s_u = []
         p_r1s_u = []
@@ -257,7 +267,7 @@ class AndOrBelief(AndBelief):
             likelihood = 1
             state_config = self.configs[s]
             z = self.num_dots - state_config.sum()
-            for i,d in enumerate(ask):
+            for i,d in enumerate(utt):
                 if d == 1:
                     disconfirm = self.likelihood[0,d,state_config[i]] * (8/9) ** z
                     likelihood *= 1 - disconfirm
@@ -299,51 +309,40 @@ class AndOrConfigBelief(AndBelief):
         for each dot in z
         Size of z: remaining dots outside of s |z| = num_dots - |s|
     """
-    def p_response(self, prior, ask):
+
+    def joint(self, prior, utt):
+        # p(r | u,s)
         # prior: num_configs * 7
-        # \sum_s p(r=0|u,s)p(s)
-        # = \sum_s,z p(s)p(z|s) p(r=0|u,s)p(r=0|u,z)
-        # = \sum_s p(s)p(r=0|u,s)\sum_z p(z|s)p(r=0|u,z)
-        # = \sum_s p(s)(1-\prod_i p(r=1|ui,si)) \sum_z p(z|s)p(r=0|u,z)
-        # = \sum_s p(s)(1-\prod_i p(r=1|ui,si)) |z|C|u|9^-|u|
-        p_r1 = 0
-        p_r0 = 0
+        # p(r=0|u,s)p(s)
+        # = \sum_z p(s)p(z|s) p(r=0|u,s)p(r=0|u,z)
+        # = p(s)p(r=0|u,s)\sum_z p(z|s)p(r=0|u,z)
+        # = p(s)(1-\prod_i p(r=1|ui,si)) \sum_z p(z|s)p(r=0|u,z)
+        # = p(s)(1-\prod_i p(r=1|ui,si)) |z|C|u|9^-|u|
+        p_r1 = []
+        p_r0 = []
         for s,ps in enumerate(prior):
             likelihood = 1
             state_config = self.configs[s]
             z = self.num_dots - state_config.sum()
-            u = int(ask.sum())
-            for i,d in enumerate(ask):
+            u = int(utt.sum())
+            for i,d in enumerate(utt):
                 if d == 1:
                     likelihood *= self.likelihood[1,d,state_config[i]]
             distractor_prob = 1 - comb(z,u) * 9. ** (-u)
-            p_r0 += (1 - likelihood)*distractor_prob * ps
-            p_r1 += (1- (1-likelihood)*distractor_prob) * ps
-        #p_r0 = 1 - p_r1 # this is equivalent
+            p_r0.append((1 - likelihood)*distractor_prob * ps)
+            p_r1.append((1- (1-likelihood)*distractor_prob) * ps)
         return np.array((p_r0, p_r1))
 
-    def posterior(self, prior, ask, response):
+    def p_response(self, prior, utt):
+        return self.joint(prior, utt).sum(1)
+
+    def posterior(self, prior, utt, response):
         # p(r=., s | u) = \prod_i p(r=. | ui, si)p(s)
-        p_r0s_u = []
-        p_r1s_u = []
-        for s,ps in enumerate(prior):
-            likelihood = 1
-            state_config = self.configs[s]
-            z = self.num_dots - state_config.sum()
-            u = int(ask.sum())
-            for i,d in enumerate(ask):
-                if d == 1:
-                    likelihood *= self.likelihood[1,d,state_config[i]]
-            distractor_prob = 1 - comb(z,u) * 9. ** (-u)
-            p_r0s_u.append((1-likelihood)*distractor_prob * ps)
-            p_r1s_u.append((1-(1-likelihood)*distractor_prob) * ps)
-        p_r1s_u = np.array(p_r1s_u)
-        p_r0s_u = np.array(p_r0s_u)
-        Z1 = p_r1s_u.sum(-1, keepdims=True)
-        p_s_ur1 = p_r1s_u / Z1 if Z1 > 0 else np.ones((2 ** self.num_dots,)) / 2 ** self.num_dots
-        Z0 = p_r0s_u.sum(-1, keepdims=True)
-        p_s_ur0 = p_r0s_u / Z0 if Z0 > 0 else np.ones((2 ** self.num_dots,)) / 2 ** self.num_dots
-        return p_s_ur1 if response == 1 else p_s_ur0
+        p_rs_u = self.joint(prior, utt)
+        Z = p_rs_u.sum(1, keepdims=True)
+        unif = np.ones_like(p_rs_u) / 2 ** self.num_dots
+        p_s_ur = np.divide(p_rs_u, Z, out=unif, where=Z>0)
+        return p_s_ur[response]
 
 
 if __name__ == "__main__":
@@ -352,7 +351,7 @@ if __name__ == "__main__":
     #num_dots = 4
     #overlap_size = None
 
-    ask = np.array([1 if x in [2,5] else 0 for x in range(num_dots)])
+    utt = np.array([1 if x in [2,5] else 0 for x in range(num_dots)])
     response = np.array([1 if x in [2,5] else 0 for x in range(num_dots)])
     configs = np.array([
         np.unpackbits(np.array([x], dtype=np.ubyte))[8 - num_dots:]
@@ -361,35 +360,42 @@ if __name__ == "__main__":
 
     def compute_EdH(belief):
         EdHs = []
-        for utt in configs[1:]:
+        for utt in configs:
             EdH = belief.expected_info_gain(belief.prior, utt)
             EdHs.append(EdH)
         return np.array(EdHs)
 
     # refactor later into test
     print("IND MODEL")
-    EdHs = compute_EdH(IndependentBelief(num_dots))
-    print(EdHs)
+    belief = IndependentBelief(num_dots)
+    EdHs = compute_EdH(belief)
+    cs, hs = belief.viz_belief(EdHs)
+    print(cs)
+    print(hs)
 
     # joint model
     print("JOINT MODEL")
-    EdHs = compute_EdH(AndBelief(num_dots, overlap_size = overlap_size))
-    print(EdHs)
-    print(EdHs.max(), EdHs.argmax(), configs[EdHs.argmax()])
+    belief = AndBelief(num_dots, overlap_size = overlap_size)
+    EdHs = compute_EdH(belief)
+    cs, hs = belief.viz_belief(EdHs)
+    print(cs)
+    print(hs)
 
     # po dot model
     print("PO DOT MODEL")
-    EdHs = compute_EdH(AndOrBelief(num_dots, overlap_size = overlap_size))
-    print(EdHs)
-    print(EdHs.max(), EdHs.argmax(), configs[EdHs.argmax()])
+    belief = AndOrBelief(num_dots, overlap_size = overlap_size)
+    EdHs = compute_EdH(belief)
+    cs, hs = belief.viz_belief(EdHs)
+    print(cs)
+    print(hs)
 
     # po config model
     print("PO CONFIG MODEL")
     belief = AndOrConfigBelief(num_dots, overlap_size = overlap_size)
-
     EdHs = compute_EdH(belief)
-    print(EdHs)
-    print(EdHs.max(), EdHs.argmax(), configs[EdHs.argmax()])
+    cs, hs = belief.viz_belief(EdHs)
+    print(cs)
+    print(hs)
 
     print("20 questions simulation")
 
@@ -406,13 +412,6 @@ if __name__ == "__main__":
     print("true state")
     print(state)
 
-    def viz_belief(belief, p, n=5):
-        # decreasing order
-        idxs = (-p).argsort()[:n]
-        cs = belief.configs[idxs]
-        ps = p[idxs]
-        return cs, ps
-
     prior = belief.prior
     for t in range(10):
         EdHs = []
@@ -422,11 +421,11 @@ if __name__ == "__main__":
         EdHs = np.array(EdHs)
         best_idx = EdHs.argmax()
         next_utt = configs[best_idx]
-        response = state.astype(bool)[next_utt.astype(bool)].all()
+        response = int(state.astype(bool)[next_utt.astype(bool)].all())
         posterior = belief.posterior(prior, next_utt, response)
         print(f"utt {next_utt}: response {response}")
         print("posterior")
-        cs, ps = viz_belief(belief, posterior)
+        cs, ps = belief.viz_belief(posterior)
         print(cs)
         print(ps)
         import pdb; pdb.set_trace()
