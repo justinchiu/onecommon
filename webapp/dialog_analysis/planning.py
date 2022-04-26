376ba6288ce1a67b1fb8e347e9c27f15fd554d0f
Justin Chiu <justinchiu@g.harvard.edu>
2022-04-25 23:33:06 -0400

    fix visualization

--- webapp/dialog_analysis/planning.py
+++ webapp/dialog_analysis/planning.py
@@ -194,27 +194,41 @@ def process_dialogue(dialogue_dict):
     visualize_dialogue(dialogue)
 """
 
-def visualize_board(left_dots, right_dots, mentions, intersect, beliefs=None):
+def visualize_board(
+    left_dots, right_dots,
+    left_mentions, right_mentions,
+    left_intersect, right_intersect,
+    left_beliefs=None, right_beliefs=None,
+):
     shift = 430
 
     left_dots_html = (map(lambda x: x.html(), left_dots)
-        if beliefs is None
-        else map(lambda x: x[0].html(value=x[1]), zip(left_dots, beliefs))
+        if left_beliefs is None
+        else map(lambda x: x[0].html(value=x[1]), zip(left_dots, left_beliefs))
     )
-    right_dots_html = map(lambda x: x.html(shift), right_dots)
+    right_dots_html = (map(lambda x: x.html(shift), right_dots)
+        if right_beliefs is None
+        else map(lambda x: x[0].html(shift, value=x[1]), zip(right_dots, right_beliefs))
+    )
 
-    mentions_html = map(lambda x: x.select_html(), mentions)
-    intersect_dots = map(lambda x: x.intersect_html(), intersect)
+    if left_mentions is not None:
+        left_mentions_html = map(lambda x: x.select_html(), left_mentions)
+    if right_mentions is not None:
+        right_mentions_html = map(lambda x: x.select_html(shift), right_mentions)
+    left_intersect_dots = map(lambda x: x.intersect_html(), left_intersect)
+    right_intersect_dots = map(lambda x: x.intersect_html(shift), right_intersect)
 
     nl = "\n"
     html = f"""
     <svg width="860" height="430">
     <circle cx="215" cy="215" r="205" fill="none" stroke="black" stroke-width="2" stroke-dasharray="3,3"/>
     {nl.join(left_dots_html)}
-    {nl.join(intersect_dots)}
-    {nl.join(mentions_html)}
+    {nl.join(left_intersect_dots)}
+    {nl.join(left_mentions_html) if left_mentions is not None else ""}
     <circle cx="645" cy="215" r="205" fill="none" stroke="black" stroke-width="2" stroke-dasharray="3,3"/>
     {nl.join(right_dots_html)}
+    {nl.join(right_intersect_dots)}
+    {nl.join(right_mentions_html) if right_mentions is not None else ""}
     </svg>
     """
     components.html(html, height=430, width=860)
@@ -222,13 +236,19 @@ def visualize_board(left_dots, right_dots, mentions, intersect, beliefs=None):
 def visualize_dialogue(dialogue):
     st.table(dialogue)
 
+def visualize_beliefs(dots, configs, belief):
+    st.write("Configs (Belief)")
+    for row, b in zip(configs, belief):
+        st.write(f'{" ".join([x.id for on, x in zip(row, dots) if on])} ({b:.4f})')
+
 def process_dialogue(scenario_id, dialogue):
     board = boards[scenario_id]
     st.write(f"Chat scenario id: {scenario_id}")
 
     b0 = [Dot(x) for x in board["kbs"][0]]
     b1 = [Dot(x) for x in board["kbs"][1]]
-    intersect = [x for x in b0 for y in b1 if x.id == y.id]
+    intersect0 = [x for x in b0 for y in b1 if x.id == y.id]
+    intersect1 = [x for x in b1 for y in b0 if x.id == y.id]
 
     #turn = st.radio("Turn number", np.arange(len(dialogue)))
     turn = st.number_input("Turn number", 0, len(dialogue)-1)
@@ -244,25 +264,59 @@ def process_dialogue(scenario_id, dialogue):
     if display == "prior":
         st.header("Prior next mentions")
         prior_mentions = turn["prior_mentions"]
-        prior_idx = st.radio("Prior number", np.arange(len(prior_mentions)))
-        mention = [x for i,x in enumerate(b0) if prior_mentions[prior_idx][i] == 1]
-        visualize_board(b0, b1, mention, intersect)
+        if prior_mentions is not None:
+            prior_idx = st.radio("Prior number", np.arange(len(prior_mentions)))
+            if turn["agent_id"] == 0:
+                mention0 = [x for i,x in enumerate(b0) if prior_mentions[prior_idx][i] == 1]
+                mention1 = None
+            elif turn["agent_id"] == 1:
+                mention1 = [x for i,x in enumerate(b1) if prior_mentions[prior_idx][i] == 1]
+                mention0 = None
+            visualize_board(b0, b1, mention0, mention1, intersect0, intersect1)
+        else:
+            st.write("No prior next mentions")
     elif display == "plan":
         st.header("Plan next mentions")
         plan_mentions = turn["plan_mentions"]
-        plan_idx = st.radio("Plan number", np.arange(len(plan_mentions)))
-        mention = [x for i,x in enumerate(b0) if plan_mentions[plan_idx][i] == 1]
-        visualize_board(b0, b1, mention, intersect)
+        if plan_mentions is not None:
+            plan_idx = st.radio("Plan number", np.arange(len(plan_mentions)))
+            if turn["agent_id"] == 0:
+                mention0 = [x for i,x in enumerate(b0) if plan_mentions[plan_idx][i] == 1]
+                mention1 = None
+            elif turn["agent_id"] == 1:
+                mention1 = [x for i,x in enumerate(b1) if plan_mentions[plan_idx][i] == 1]
+                mention0 = None
+            visualize_board(b0, b1, mention0, mention1, intersect0, intersect1)
+        else:
+            st.write("No plan mentions")
     elif display == "posterior":
         st.header("Input and belief posterior")    
         st.write(f"Input utterance: {turn['utterance_language']} || response: {turn['response_language']}")
-        mention = [x for i,x in enumerate(b0) if turn["utterance"][i] == 1]
-        visualize_board(b0, b1, mention, intersect, beliefs=turn["marginal_belief"])
+        if turn["agent_id"] == 0:
+            mention0 = (
+                [x for i,x in enumerate(b0) if turn["utterance"][i] == 1]
+                if turn["utterance"] is not None else None
+            )
+            mention1 = None
+        elif turn["agent_id"] == 1:
+            mention1 = (
+                [x for i,x in enumerate(b1) if turn["utterance"][i] == 1]
+                if turn["utterance"] is not None else None
+            )
+            mention0 = None
+        visualize_board(
+            b0, b1, mention0, mention1, intersect0, intersect1,
+            left_beliefs=turn["marginal_belief"] if turn["agent_id"] == 0 else None,
+            right_beliefs=turn["marginal_belief"] if turn["agent_id"] == 1 else None,
+        )
+        visualize_beliefs(b0 if turn["agent_id"] == 0 else b1, turn["configs"], turn["belief"])
 
 
 
 dir = "../../aaai2020/experiments/analysis_log"
 scenario_id = "S_pGlR0nKz9pQ4ZWsw"
+scenario_id = "S_n0ocL412kqOAl9QR"
+
 filepath = (Path(dir) / scenario_id).with_suffix(".json")
 
 with filepath.open() as f:
