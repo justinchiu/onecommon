63b03fbd50f3b18ed56817cac648341a8da90770
Justin Chiu <chiu.justin.t@gmail.com>
2022-05-05 00:55:34 -0400

    save pretrained models, nli not good

--- response_annotation/train_response.py
+++ response_annotation/train_response.py
@@ -11,6 +11,21 @@ from datasets import load_metric
 
 from response import ResponseDb
 
+import random
+
+# constants
+SENTIMENT = "sentiment"
+NLI = "nli"
+
+datatype = SENTIMENT
+#datatype = NLI
+
+# seeds
+seed = 1111
+torch.manual_seed(seed)
+random.seed(seed)
+np.random.seed(seed)
+
 class SimpleDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
         self.encodings = encodings
@@ -26,7 +41,7 @@ class SimpleDataset(torch.utils.data.Dataset):
 
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
-    num_train_epochs=10,             # total number of training epochs
+    num_train_epochs=15,             # total number of training epochs
     per_device_train_batch_size=16,  # batch size per device during training
     per_device_eval_batch_size=64,   # batch size for evaluation
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
@@ -44,7 +59,11 @@ model = AutoModelForSequenceClassification.from_pretrained(
 
 db = ResponseDb()
 data = db.get_all()
-texts = [x[-2] for x in data]
+texts = None
+if datatype == SENTIMENT:
+    texts = [x[-2] for x in data]
+elif datatype == NLI:
+    texts = [f"{x[-3]} </s></s> {x[-2]}" for x in data]
 labels = [x[-1] for x in data]
 tokenized_texts = tokenizer(texts,truncation=True,padding=True)
 
@@ -61,8 +80,8 @@ valid_dataset = SimpleDataset(valid_tokenized_texts, valid_labels)
 trainer = Trainer(
     model=model,                         # the instantiated 🤗 Transformers model to be trained
     args=training_args,                  # training arguments, defined above
-    train_dataset=train_dataset,          # training dataset
-    eval_dataset=valid_dataset,           # evaluation dataset
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=valid_dataset,          # evaluation dataset
 )
 
 trainer.train()
@@ -73,3 +92,8 @@ preds = np.argmax(predictions.predictions, axis=-1)
 metric = load_metric("accuracy")
 accuracy = metric.compute(predictions=preds, references=predictions.label_ids)
 print(accuracy)
+
+save_directory = "./save_pretrained"
+tokenizer.save_pretrained(save_directory)
+model.save_pretrained(save_directory)
+#trainer.save_model(f"response-model-{datatype}-{accuracy:.2f}")
