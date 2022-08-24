746c69681897f3854d530fc35251d169b233ded2
Justin Chiu <chiu.justin.t@gmail.com>
2022-08-24 10:17:28 -0400

    add absolute bucketing arg back to static test script

--- aaai2020/experiments/jc_run_test_static_plan.sh
+++ aaai2020/experiments/jc_run_test_static_plan.sh
@@ -71,7 +71,7 @@ python -u test_planning_static.py \
   --alice_model_file=${model_dir_a}/1_ep-12.th \
   --bob_model_file=${model_dir_b}/1_ep-12.th \
   --belief ${belief} \
-  --absolute_bucketing \
+  --absolute_bucketing 1 \
   --context_file=${context_file} \
   --cuda \
   --markable_detector_file=serialized_models/markable_detector_with_dict_1.th \
